0d9281143694861e1544c122834dbc50cdf61486
Nicolas Battisti <nickgabrielbd@gmail.com>
2024-01-23 17:37:04 -0300

    feat: improved install script

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 
+# Copying the monitoring script
+sudo cp monitor.sh /usr/local/bin/monitor.sh
+
 # Install the service on the machine
 if [ ! -e /etc/systemd/system/prometheus.service ]; then
+  echo "Registering third party service - Process Monitoring Service"
   sudo cp pcmonitoring.service /etc/systemd/system/
   sudo systemctl daemon-reload
   sudo systemctl enable pcmonitoring
   sudo systemctl start pcmonitoring
 fi
+
+# reload the daemon
+sudo systemctl daemon-reload
+
